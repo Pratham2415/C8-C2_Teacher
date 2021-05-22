adc846a0b4e06a85c0966d6a184d408f987ba5b1
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-05-22 16:46:26 +0530

    Add files via upload

---
+++ Sol_TA_Rectangle.py
@@ -0,0 +1,12 @@
+import turtle
+
+length = 200
+breadth = 300
+
+
+for i in range(2): #write the number for which loop has to be repeated
+	turtle.forward(length)
+	turtle.left(90)
+	turtle.forward(breadth)
+	turtle.left(90)
+		
---
+++ Sol_TA_Square.py
@@ -0,0 +1,8 @@
+import turtle
+
+side = 200
+
+
+for i in range(4): #write the number for which loop has to be repeated
+	turtle.forward(side)
+	turtle.left(90)
